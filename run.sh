3540d1969114a451d5e90c35c1bd3bd143b26817
Stefan Reuter <stefan.reuter@reucon.com>
2017-11-27 19:53:59 +0100

    Update for Sonar 6.7

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 set -e
 
 if [ "${1:0:1}" != '-' ]; then
-	exec "$@"
+  exec "$@"
 fi
 
 if [ -z "$CROWD_URL" ]; then
@@ -11,11 +12,13 @@ else
 	CROWD_OPTS="-Dsonar.security.realm=Crowd -Dcrowd.url=${CROWD_URL} -Dcrowd.application=${CROWD_APPLICATION} -Dcrowd.password=${CROWD_PASSWORD}"
 fi
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+chown -R sonarqube:sonarqube $SONARQUBE_HOME
+exec gosu sonarqube \
+  java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  $CROWD_OPTS \
-  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
-	"$@"
+  $CROWD_URL
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "$@"
